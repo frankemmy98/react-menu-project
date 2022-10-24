3e9251e27e8cf8fcf469d8508296bea40c5d3212
frankemmy98 <emmyfrank349@gmail.com>
2022-10-24 08:23:10 -0500

    changed build in package.json

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState(allCategories);
+  console.log(setCategories);
 
   const filterItems = (category) => {
     if (category === "all") {
